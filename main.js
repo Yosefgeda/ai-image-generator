6ca7ef149b172f3f00a9e511c02119a180b87b62
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-13 13:35:29 +0300

    Connect to api

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 const generateForm = document.querySelector('.generate-form');
 const imageGallery = document.querySelector('.image-gallery');
+
+const apiKey = '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';
+const generateAiImages = async (userPrompt, userImgQuantity) => {
+    try {
+        const response = await fetch("https://api.openai.com/v1/images/generations", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Authorization": `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            prompt: userPrompt,
+            n: parseInt(userImgQuantity),
+            size: "512x512",
+            response_format: "b64_json"
+        })
+    });
+    if(!response.ok) throw new Error("Failed to generate images! Please try again.");
+
+    const data = await response.json()
+    console.log(data)
+    } catch (error) {
+        alert(error.message);
+    }
+}
 const handleSubmisson = (e) => {
     e.preventDefault();
     const userPrompt = e.srcElement[0].value;
@@ -16,6 +41,8 @@ const handleSubmisson = (e) => {
     imageGallery.innerHTML = '';
     for(let i = 0; i < userImgQuantity; i++){
         imageGallery.innerHTML += imgMarkup;
-    }
+    };
+
+    generateAiImages(userPrompt, userImgQuantity);
 } 
 generateForm.addEventListener('submit', handleSubmisson);
