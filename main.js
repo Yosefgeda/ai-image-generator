97d49088cc64c620bb606724e98bd6f8a27cf2ad
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-13 12:52:28 +0300

    generate image loading as per user input value

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const generateForm = document.querySelector('.generate-form');
-
+const imageGallery = document.querySelector('.image-gallery');
 const handleSubmisson = (e) => {
     e.preventDefault();
-    console.log(e.srcElement)
+    const userPrompt = e.srcElement[0].value;
+    const userImgQuantity = e.srcElement[1].value;
+
+    let imgMarkup = `
+        <div class="img-card loading">
+            <img src="images/loader.svg" alt="Hot air ballon"/>
+            <a href="#" class="download-btn">
+                <img src="images/download.svg" alt="download"/>
+            </a>
+        </div>
+    `;
+    imageGallery.innerHTML = '';
+    for(let i = 0; i < userImgQuantity; i++){
+        imageGallery.innerHTML += imgMarkup;
+    }
 } 
 generateForm.addEventListener('submit', handleSubmisson);
