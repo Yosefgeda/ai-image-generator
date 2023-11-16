042763b3afbd4dcf61b8126c104fa25c490a5aa7
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-16 13:12:01 +0300

    Changed api

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const generateForm = document.querySelector('.generate-form');
 const imageGallery = document.querySelector('.image-gallery');
 
-const apiKey = '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';
+const apiKey = 'k65he_gPlyR76ovg_qq9esbN_O7BmjJwtfWTTVov65g';
+
 const updateImageCard = (imgDataArray) => {
     imgDataArray.forEach((imgObject, index) => {
         const imgCard = imageGallery.querySelectorAll('.img-card')[index];
@@ -17,50 +18,34 @@ const updateImageCard = (imgDataArray) => {
             downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
         }
     });
+    console.log(imgDataArray);
 }
 
 const generateAiImages = async (userPrompt, userImgQuantity) => {
-    try {
-        const response = await fetch("https://api.openai.com/v1/images/generations", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "Authorization": `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-            prompt: userPrompt,
-            n: parseInt(userImgQuantity),
-            size: "512x512",
-            response_format: "b64_json"
-        })
-    });
-    // if(!response.ok) throw new Error("Failed to generate images! Please try again.");
-
-    const { data } = await response.json()
-    updateImageCard([...data]);
-    } catch (error) {
-        console.log(error)
-        alert(error.message);
-    }
-}
-const handleSubmisson = (e) => {
-    e.preventDefault();
-    const userPrompt = e.srcElement[0].value;
-    const userImgQuantity = e.srcElement[1].value;
-
-    let imgMarkup = `
-        <div class="img-card loading">
-            <img src="images/loader.svg" alt="Hot air ballon"/>
+    const response = await fetch(`https://api.unsplash.com/search/photos?
+            page=${userImgQuantity}&query=${userPrompt}&client_id=${apiKey}`
+        );
+    const data = await response.json();
+    const res = data.results;
+    imageGallery.innerHTML = '';
+    let imgMarkup = res.map(itm =>
+        `
+        <div class="img-card">
+            <img src="${itm.urls.full}" alt=${res.alt_description} class="generated-images"/>
             <a href="#" class="download-btn">
                 <img src="images/download.svg" alt="download"/>
             </a>
         </div>
-    `;
-    imageGallery.innerHTML = '';
-    for(let i = 0; i < userImgQuantity; i++){
-        imageGallery.innerHTML += imgMarkup;
-    };
+    `
+    ).join('');
+    imageGallery.innerHTML += imgMarkup;
+    updateImageCard(res);
+}
 
+const handleSubmisson = (e) => {
+    e.preventDefault();
+    const userPrompt = e.srcElement[0].value;
+    const userImgQuantity = e.srcElement[1].value;
     generateAiImages(userPrompt, userImgQuantity);
 } 
 generateForm.addEventListener('submit', handleSubmisson);
